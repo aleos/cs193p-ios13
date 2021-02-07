e8d3dbfc54a17f780d1f00ba740ea25e23f3461e
Alexander Ostrovsky <thealeos@gmail.com>
2021-02-07 12:09:26 +0300

    Add Memorize project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alexander Ostrovsky on 07.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
